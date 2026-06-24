dd9e0467c4d7277fde95033bf5ab1df4c0234df6
Kanika Dadhich <kanikadadhich70@gmail.com>
2026-06-24 20:37:37 +0530

    Time: 0 ms (100%), Space: 28.3 MB (27.9%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    int majorityElement(vector<int>& nums) 
+    {
+        int candidate = nums[0];
+        int count=1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(candidate == nums[i])
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+                if(count==0)
+                {
+                    candidate=nums[i];
+                    count=1;
+                }
+            }
+        }
+        return candidate;
+    }
+};
