59cd2d44dafe8a6c9e8262f46381bf6d29b9144f
Kanika Dadhich <kanikadadhich70@gmail.com>
2026-07-03 13:52:00 +0530

    Time: 0 ms (100%), Space: 7.9 MB (68%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -1,8 +1,6 @@
-class Solution 
-{
-    public:
-    int hammingDistance(int x, int y) 
-    {
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
         return __builtin_popcount(x^y);
     }
 };
